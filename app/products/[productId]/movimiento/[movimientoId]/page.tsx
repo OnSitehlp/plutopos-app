e1bd99d8ddcaf07849a12303e18e48ce6ce87a5b
OnSitehlp <patcbox@gmail.com>
2024-03-09 18:16:29 -0300

    delete product folder

--- app/products/[productId]/movimiento/[movimientoId]/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function TransactionDetail({ params }:{
-    params: {
-        productId: string,
-        movimientoId: string,
-    };
-}) {
-    return (<h1> Movimiento { params.movimientoId } del producto { params.productId} </h1>);
-}
